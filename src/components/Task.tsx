835e85e90cdf1483b0b285a58691a39c32c7d10f
Jack Marshall <37175625+JackM510@users.noreply.github.com>
2026-06-23 09:55:46 +1000

    created a '+' button for adding a new task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,17 +1,21 @@
 "use client"; // Run in the browser
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { TaskType } from "../types/task"
-import { TrashIcon } from "@heroicons/react/24/outline";
+import { CheckIcon, TrashIcon } from "@heroicons/react/24/outline";
 
 type TaskProps = {
     task: TaskType;
+    isNew?: boolean;
     /*onToggle: (id: string) => void;
     onEdit: (id: string) => void;
     onDelete: (id: string) => void;*/
 }
 
-export default function Task({ task }: TaskProps) {
-    const [isEditing, setIsEditing] = useState(false);
+export default function Task({ task, isNew }: TaskProps) {
+
+    const router = useRouter();
+    const [isEditing, setIsEditing] = useState(isNew || false);
     const [draftTitle, setDraftTitle] = useState(task.title);
     const priorityClasses = {
         high: "bg-red-500 text-white",
@@ -19,26 +23,61 @@ export default function Task({ task }: TaskProps) {
         low: "bg-yellow-500 text-white"
     }
 
-    return (
-        <div className="flex p-4 bg-blue-100 border rounded mb-2">
+    // Create new task
+    async function handleCreate() {
+        const res = await fetch("/api", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                title: draftTitle,
+                completed: false,
+                priority: null
+            })
+        });
+        if (!res.ok) console.error("Failed to create task");
+        isNew = false;
+        router.refresh(); // Refresh task list after saving
+    }
 
+    // Update existing task
+    async function handleUpdate() {
+        const res = await fetch (`/api/$task._id}`, {
+            method: "PATCH",
+            headers: {"Content-Type": "applicaiton/JSON"},
+            body: JSON.stringify({ title: draftTitle })
+        });
+        if (!res.ok) console.error("Failed to update task");
+        setIsEditing(false);
+        router.refresh(); // Refresh task list after saving
+    }
+
+    return (
+        <div className="flex justify-between p-4 bg-gray-100 border rounded mb-2">
             {/* Left side: checkbox + text column */}
-            <div className="flex items-baseline gap-2">
+            <div className="flex flex-1 items-baseline">
                 {/* Checkbox */}
                 <input
                     type="checkbox"
-                    
+                    className="mr-4"
                     checked={task.completed}
                     onChange={() => alert(":)")}
                 />
 
                 {/* Title + priority */}
-                <div className="flex flex-col">
+                <div className="flex flex-1 flex-col">
                     {isEditing ? (
                     <input
                         value={draftTitle}
+                        placeholder="Enter a task"
+                        autoFocus={isNew}
                         onChange={(e) => setDraftTitle(e.target.value)}
-                        onBlur={() => setIsEditing(false)}
+                        onBlur={() => {
+                            if (!isNew) {
+                                setIsEditing(false);
+                                setDraftTitle(task.title);
+                            }
+                            }}
+
                         className="
                             text-lg leading-6 text-gray-700
                             border-b border-gray-300
@@ -61,10 +100,18 @@ export default function Task({ task }: TaskProps) {
                     </span>
                 </div>
             </div>
-
             {/* Right side: delete button */}
-            <div className="flex items-baseline justify-conten">
-                <TrashIcon className="h-5 w-5 text-gray-700 hover:opacity-80"/>
+            <div className="flex items-baseline ml-4">
+                {isNew || isEditing ? (
+                    <CheckIcon 
+                        className="h-5 w-5 text-green-500 hover:opacity-80"
+                        onClick={() => isNew ? handleCreate() : handleUpdate() }
+                    />
+                ) : (
+                    <TrashIcon 
+                        className="h-5 w-5 text-gray-700 hover:opacity-80"
+                    />
+                )}
             </div>
         </div>
     );
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,22 +1,46 @@
 "use client";
+import { useState } from "react";
 import Task from "./Task"
 import { TaskType } from "../types/task"
+import { PlusIcon } from "@heroicons/react/24/outline";
 
 type taskListProps= {
     tasks: TaskType[]
 }
 
 export default function TaskList({ tasks }: taskListProps ) {
+    const [isAdding, setIsAdding] = useState(false);
+
     return (
         <div className="w-1/2 mx-auto p-12">
             {/* Heading */}
-            <div className="border-b-3 border-gray-300 mb-4">
-                <h1 className="text-5xl font-bold text-black pb-4">
+            <div className="flex items-baseline justify-between border-b-3 border-gray-300 mb-4">
+                <h1 className="text-4xl font-bold text-black pb-4">
                     Task Manager
                 </h1>
+                <PlusIcon 
+                    className="h-5 w-5 text-black"
+                    onClick={() => setIsAdding(true)}
+                />
             </div>
-            {/* List of Tasks */}
+            {/* Tasks */}
             <div>
+                {/* Add Task */}
+                {isAdding && (
+                    <Task
+                        task={{
+                            _id: "new-task",
+                            title:"Enter a task",
+                            completed: false,
+                            priority: "low"
+                        }}
+                        isNew={true}
+                    />
+                )}
+
+
+
+                {/* Existing Tasks */}
                 {tasks.map((task) => (
                     < Task
                         key={task._id}

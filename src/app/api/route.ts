e213b5fd15e8ff7d8a92b9cdcef1a3bafb695629
Jack Marshall <37175625+JackM510@users.noreply.github.com>
2026-06-23 08:23:08 +1000

    added a POST for inserting a new task into MongoDB

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -6,3 +6,11 @@ export async function GET() {
     const tasks = await db.collection("tasks").find({}).toArray(); // Fetch all tasks
     return Response.json(tasks, { status: 200 }); // Return tasks as JSON with 200 OK status
 }
+
+export async function POST(req: Request) {
+    const client = await clientPromise;
+    const db = client.db(process.env.MONGODB_DB);
+    const data = await req.json();  // Read JSON sent in POST request and create as JS object
+    const result = db.collection("tasks").insertOne(data); // Insert
+    return Response.json(result, { status: 201 }); // Send response confirming insert with HTTP 201 (Created) 
+}

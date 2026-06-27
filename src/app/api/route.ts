34392a125e0d3a53cf0f938cf6faf5adf5dc6484
Jack Marshall <37175625+JackM510@users.noreply.github.com>
2026-06-27 12:28:46 +1000

    added a .sort call to tasks fetch statement to filter incomplete/completed tasks

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -4,7 +4,7 @@ import clientPromise from "@/lib/mongodb";
 export async function GET() {
     const client = await clientPromise; // Wait for MongoDB to finish connecting
     const db = client.db(process.env.MONGODB_DB); // Select the DB from .env-local
-    const tasks = await db.collection("tasks").find({}).toArray(); // Fetch all tasks
+    const tasks = await db.collection("tasks").find({}).sort({completed: 1}).toArray(); // Fetch all tasks
     return Response.json(tasks, { status: 200 }); // Return tasks as JSON with 200 OK status
 }
 
